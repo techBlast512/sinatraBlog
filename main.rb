0aa811129de30ecb79c796438fdf62c31b8df761
techblast512 <techblast512@gmail.com>
2015-12-01 23:45:11 -0600

    added database configuration

--- main.rb
+++ main.rb
@@ -5,6 +5,14 @@ require 'sinatra/reloader' if development?
 set :public_folder, 'assets'
 set :views, 'templates'
 
+configure :development do
+	DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+	DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
 set :session_secret, 'itsGonnaBeHardToCrackThisOne'
 
 get '/login' do
